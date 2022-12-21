649c0b925b36887b7f8f1b31400eb3d160f9021a
Alice0628 <104606717+Alice0628@users.noreply.github.com>
2022-12-21 15:54:55 -0500

    Update Project.jsx

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -9,56 +9,13 @@ import IMG6 from '../../assets/portfolio6.jpg'
 import { PersonContext } from '../../App'
 import { useContext } from 'react'
 
-// DO NOT USE THE IMAGES IN PRODUCTION
-
-// const data = [
-//   {
-//     id: 1,
-//     image: IMG1,
-//     title: 'Crypto Currency Dashboard & Financial Visualization',
-//     github: 'https://github.com',
-//     demo: 'https://dribbble.com/shots/16673715-Crypto-currency-dashboards-and-financial-data-visualization'
-//   },
-//   {
-//     id: 2,
-//     image: IMG2,
-//     title: 'Charts templates & infographics in Figma',
-//     github: 'https://github.com',
-//     demo: 'https://dribbble.com/shots/16580766-Orion-UI-kit-Charts-templates-infographics-in-Figma'
-//   },
-//   {
-//     id: 3,
-//     image: IMG3,
-//     title: 'Figma dashboard UI kit for data design web apps',
-//     github: 'https://github.com',
-//     demo: 'https://dribbble.com/shots/17290917-Eclipse-Figma-dashboard-UI-kit-for-data-design-web-apps'
-//   },
-//   {
-//     id: 4,
-//     image: IMG4,
-//     title: 'Maintaining tasks and tracking progress',
-//     github: 'https://github.com',
-//     demo: 'https://dribbble.com/shots/16955822-Maintaining-tasks-and-tracking-progress'
-//   },
-//   {
-//     id: 5,
-//     image: IMG5,
-//     title: 'Charts templates & infographics in Figma',
-//     github: 'https://github.com',
-//     demo: 'https://dribbble.com/shots/16541289-Orion-UI-kit-Charts-templates-infographics-in-Figma'
-//   },
-//   {
-//     id: 6,
-//     image: IMG6,
-//     title: 'Charts templates & infographics in Figma',
-//     github: 'https://github.com',
-//     demo: 'https://dribbble.com/shots/15887665-Orion-UI-kit-Charts-templates-infographics-in-Figma'
-//   }
-// ]
-
 
 const Project = () => {
   const [
+    info,
+    setInfo,
+    aboutPhoto,
+    setAboutPhoto,
     experience,
     setExperience,
     header,
@@ -67,6 +24,8 @@ const Project = () => {
     setEducation,
     projects,
     setProjects,
+    cv,
+    setCv
    ] = useContext(PersonContext)
 
 
@@ -80,9 +39,9 @@ const Project = () => {
          projects.map(({id,image,title,github,demo}) => {
             return (
               <article key={id} className='portfolio__item'>
-              <div className="portfolio__item-image">
-                <img src={image} alt={title} />
-              </div>
+                {image &&<div className="portfolio__item-image">
+               <img src={`http://localhost:3001/portfolio/portfolios/getphoto/${image}`} alt={title} />
+              </div>}
               <h3>{title}</h3>
               <div className="portfolio__item-cta">
                 <a href={github} className='btn' target='_blank'>Github</a>
