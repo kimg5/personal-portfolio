e702a4a7a1cbae6025b397305c24ff04283627fa
Alice0628 <104606717+Alice0628@users.noreply.github.com>
2022-12-21 23:30:30 -0500

    Update Education.jsx

--- src/components/educations/Education.jsx
+++ src/components/educations/Education.jsx
@@ -21,27 +21,7 @@ const Education = () => {
     cv,
     setCv
    ] = useContext(PersonContext)
-  // const education = {
-  //   title: "What about my education",
-  //   subtitle: "Educations",
-
-  //   items: [
-  //     {
-  //       school: "Bachelor.Concordia University",
-  //       subject: "Computer Science",
-  //       duration: "09/2000 - 06/2004",
-  //       courses: ["Programming1", "Database", "Web development1"],
-  //       gpa: "4.0",
-  //     },
-  //     {
-  //       school: "Master. Concordia University",
-  //       subject: "Computer Science",
-  //       duration: "09/2004 - 06/2007",
-  //       courses: ["Programming2", "Data Structure", "Web development2"],
-  //       gpa: "3.8",
-  //     }
-  //   ]
-  // }
+  
 
 
   return (
@@ -52,7 +32,7 @@ const Education = () => {
 
       <div className="container education__container">
         {education.length > 0 && education.map(({school,degree, subject, country, startTime, endTime,id }) => {
-          return  <EducationDetails  school={school} subject={subject} degree = {degree} startTime={startTime} endTime={endTime} country={country} key={id} />  ;         
+          return  <EducationDetails  school={school} subject={subject} degree = {degree} startTime={startTime.slice(0,10)} endTime={endTime.slice(0,10)} country={country} key={id} />  ;         
         })}      
       </div>
     </section>
